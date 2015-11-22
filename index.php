b0cb6a4699222aea0527d12364af54fb5496c45d
Gordon <gordon@murrion.com>
2015-11-22 11:04:53 +0000

    Handle no posted data

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ require_once('bootstrap.php');
 
 $postedData = $stubs->receivePostedData();
 
-$binary = base64_decode($postedData['imageContent']);
-header('Content-Type: bitmap; charset=utf-8');
+if ($postedData['imageContent'] != '') {
+    $binary = base64_decode($postedData['imageContent']);
+    header('Content-Type: bitmap; charset=utf-8');
 
-$stubs->saveLocalFile($postedData['filename'], $binary);
+    $stubs->saveLocalFile($postedData['filename'], $binary);
 
 //$tesseract = new TesseractOCR('images/' . $filename);
 //$tesseract->setLanguage('eng');
 //$text = $tesseract->recognize();
 
-echo "Image uploaded successfully";
+    echo "Image uploaded successfully";
+} else {
+    echo "Nothing to do";
+}
